c6e1eb0bf3f4dd32958381eb83d087e93c7133f5
Arina-Vas <mrs.a.vasilevskaya@gmail.com>
2025-03-25 17:29:04 +0100

    HW-9-styles

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -53,21 +53,14 @@ function Clock() {
         day: "numeric"
     });
 
-    let formatterDay = new Intl.DateTimeFormat("en", {
-        weekday: "long"
-    });
-
-    let formatterMonth = new Intl.DateTimeFormat("en", {
-        month: "long",
-    });
 
 
     const stringTime = formatterTime.format(date) || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
     const stringDate = formatterDate.format(date) || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-    const stringDay = formatterDay.format(date) || <br/> // пишут студенты
-    const stringMonth = formatterMonth.format(date) || <br/> // пишут студенты
+    const stringDay = date.toLocaleDateString('en',{weekday: "long" })|| <br/> // пишут студенты
+    const stringMonth = date.toLocaleDateString('en',{ month: "long"}) || <br/> // пишут студенты
 
     return (
         <div className={s.clock}>
